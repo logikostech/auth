8f831d9112b1147d6dd3f75b0f001ef896fae17d
tempcke <t.bitbucket@logikostech.net>
2016-06-27 16:16:43 -0400

    added getEmail(), getPrimaryEmail(), lookupUserByUsername(), lookupUserByEmail()

--- src/UserModelInterface.php
+++ src/UserModelInterface.php
@@ -4,14 +4,20 @@ namespace Logikos\Auth;
 interface UserModelInterface {
   public function setUsername($username);
   public function setPassword($password);
+  public function addEmail($email);
   
   public function getUsername();
-  public function getPassword();  
+  public function getPassword();
+  public function getPrimaryEmail();
+  
   /**
    * @param string $username
    * @return UserModelInterface
    */
   public function getUserByLogin($login);
+  
+  public function lookupUserByUsername($username);
+  public function lookupUserByEmail($email);
   
   /**
    * @return UserModelInterface
--- tests/Mock/Users.php
+++ tests/Mock/Users.php
@@ -4,6 +4,14 @@ namespace Logikos\Tests\Mock;
 use Logikos\Auth\UserModelInterface;
 use Exception;
 
+/**
+ * Obvious this is a mock model, so dont just copy the code to use as a real model
+ * as user records will expire with the shell process.
+ * it is assumed that real models will store user info in a database somehow
+ * you may want email's stored in the same table with the users or different table
+ * its up to you!
+ * @author tempcke
+ */
 class Users implements UserModelInterface {
   
   protected static $db = [
@@ -12,7 +20,7 @@ class Users implements UserModelInterface {
   protected $newrec = [
       'username' => null,
       'password' => null,
-      'email'    => null,
+      'email'    => [],
       'user_id'  => null
   ];
   
@@ -27,28 +35,62 @@ class Users implements UserModelInterface {
   public function setPassword($password) {
     $this->record->password = $password;
   }
+  public function addEmail($email) {
+    $this->record->email[] = $email;
+  }
   public function getUsername() {
     return $this->record->username;
   }
   public function getPassword() {
     return $this->record->password;
   }
+  public function getPrimaryEmail() {
+    return isset($this->record->email[0])
+      ? $this->record->email[0]
+      : null;
+  }
   public function getUserByLogin($login) {
     $login  = strtolower($login);
     $record = null;
     foreach(self::$db as $user) {
-      if (isset($user->email) && strtolower($user->email) === $login) {
-        $record = $user; break;
+      if (isset($user->email)) {
+        foreach($user->email as $email) {
+          if (strtolower($email) === $login) {
+            $record = $user; break;
+          }
+        }
       }
-      elseif (strtolower($user->username) === $login) {
+      if (strtolower($user->username) === $login) {
         $record = $user; break;
       }
     }
     return $record
       ? $this->loadRecord($record)
       : false;
-      
   }
+  
+  public function lookupUserByUsername($username) {
+    return isset(self::$db[$username])
+      ? self::$db[$username]
+      : false;
+  }
+  public function lookupUserByEmail($lookupemail) {
+    $record = false;
+    if (!empty($lookupemail)) {
+      $lookupemail  = strtolower($lookupemail);
+      foreach(self::$db as $user) {
+        if (isset($user->email)) {
+          foreach($user->email as $email) {
+            if (strtolower($email) === $lookupemail) {
+              $record = $user; break;
+            }
+          }
+        }
+      }
+    }
+    return $record;
+  }
+  
   public function save(){
     if (empty($this->record->username) || empty($this->record->password))
       throw new Exception('username and password are required');
