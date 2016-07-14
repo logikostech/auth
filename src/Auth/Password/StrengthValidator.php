18d11a64c98b9aacd24f2da24821b6469d1a6bea
tempcke <t.bitbucket@logikostech.net>
2016-07-14 10:59:50 -0400

    changed psr4 namespace pointing at src to just Logikos instead of Logikos\Auth and so moved all files in src to src/Auth

---
+++ src/Auth/Password/StrengthValidator.php
@@ -0,0 +1,10 @@
+<?php
+namespace Logikos\Validation\Validator;
+
+use Phalcon\Validation;
+use Phalcon\Validation\Message;
+use Phalcon\Validation\Validator;
+
+class PasswordStrength extends Validator {
+  
+}
